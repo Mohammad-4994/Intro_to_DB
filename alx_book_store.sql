91b87b2d8628c921e46580f9976b997ff4492642
Mohammad-4994 <mohammadaly494@gmail.com>
2024-10-20 04:16:31 +0300

    Update alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -2,8 +2,8 @@ CREATE DATABASE IF NOT EXISTS alx_book_store ;
 
 CREATE TABLE Books (
 
-book_id int primary key ,
-title varchar(130) not null ,
+book_id int PRIMARY KEY ,
+title VARCHAR(130) not null ,
 author_id int not null ,
 price double not null ,
 publication_date DATE
@@ -13,32 +13,32 @@ publication_date DATE
 CREATE TABLE Authors (
 
 author_id int PRIMARY KEY ,
-author_name varchar(215) not null
+author_name VARCHAR(215) not null
 
 );
 
 
 CREATE TABLE Customers (
 
-customer_id int primary key ,
-customer_name varchar(215) not null,
-email varchar(215) unique,
-address text 
+customer_id int PRIMARY KEY ,
+customer_name VARCHAR(215) ,
+email VARCHAR(215) ,
+address TEXT 
 
 );
 
 CREATE TABLE Orders (
 
-order_id int primary key ,
-customer_id varchar(215) not null,
-email varchar(215) unique ,
+order_id int PRIMARY KEY ,
+customer_id VARCHAR(215) not null,
+email VARCHAR(215) unique ,
 address text 
 
 );
 
 CREATE TABLE Order_Details (
 
-orderdetailid int primary key ,
+orderdetailid int PRIMARY KEY ,
 order_id int not null,
 book_id int not null ,
 quantity DOUBLE
