650ace659be48452a982e29e7f4571a59b707ecd
Mohammad-4994 <mohammadaly494@gmail.com>
2024-10-20 04:25:43 +0300

    Update alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -29,17 +29,16 @@ address TEXT
 
 CREATE TABLE Orders (
 
-order_id int PRIMARY KEY ,
-customer_id VARCHAR(215) not null,
-email VARCHAR(215) unique ,
-address text 
+order_id INT PRIMARY KEY ,
+customer_id INT ,
+order_date DATE
 
 );
 
 CREATE TABLE Order_Details (
 
 orderdetailid int PRIMARY KEY ,
-order_id int not null,
+order_id INT not null,
 book_id int not null ,
 quantity DOUBLE
 
