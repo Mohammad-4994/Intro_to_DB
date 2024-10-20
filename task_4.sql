06415cb8eda2f4592f6e2c1c65891f6f4669de04
Mohammad-4994 <mohammadaly494@gmail.com>
2024-10-20 07:53:57 +0300

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -56,7 +56,7 @@ SELECT
     COLUMN_DEFAULT AS 'Default',
     EXTRA AS 'Extra'
 FROM 
-    information_schema.COLUMNS
+    INFORMATION_SCHEMA.COLUMNS
 WHERE 
-    TABLE_NAME = 'books'
-    AND TABLE_SCHEMA = DATABASE();
+    TABLE_NAME = 'Books'
+    AND TABLE_SCHEMA = 'alx_book_store' ;
