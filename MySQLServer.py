b22d1da0b45c8e2f22690053bf617e142e9e58ce
Mohammad Ali <mohammadaly494@gmail.com>
2024-10-20 05:33:51 +0200

    added sql-connector library

---
+++ MySQLServer.py
@@ -0,0 +1,11 @@
+import mysql.connector
+
+mydb= mysql.connector.connect(
+
+    host = "localhost",
+    user = "host",
+    password = "Pleasestay@home24",
+    database = "alx_book_store"
+
+)
+
--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,9 +1,9 @@
-CREATE DATABASE	alx_book_store ;
+CREATE DATABASE IF NOT EXISTS alx_book_store ;
 
 CREATE TABLE Books (
 
-book_id int primary key ,
-title varchar(130) not null ,
+book_id int PRIMARY KEY ,
+title VARCHAR(130) not null ,
 author_id int not null ,
 price double not null ,
 publication_date DATE
@@ -12,35 +12,36 @@ publication_date DATE
 
 CREATE TABLE Authors (
 
-author_id int primary key ,
-author_name varchar(215) not null
+author_id int PRIMARY KEY ,
+author_name VARCHAR(215) not null
 
 );
 
 
 CREATE TABLE Customers (
 
-customer_id int primary key ,
-customer_name varchar(215) not null,
-email varchar(215) unique,
-address text 
+customer_id int PRIMARY KEY ,
+customer_name VARCHAR(215) ,
+email VARCHAR(215) ,
+address TEXT 
 
 );
 
 CREATE TABLE Orders (
 
-order_id int primary key ,
-customer_id varchar(215) not null,
-email varchar(215) unique ,
-address text 
+order_id INT PRIMARY KEY ,
+customer_id INT ,
+order_date DATE ,
+FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 
 );
 
 CREATE TABLE Order_Details (
 
-orderdetailid int primary key ,
-order_id int not null,
-book_id int not null ,
+orderdetailid INT PRIMARY KEY ,
+FOREIGN KEY (order_id) REFERENCES Orders(order_id) ,
+book_id INT ,
+FOREIGN KEY (book_id) REFERENCES Books(book_id) ,
 quantity DOUBLE
 
 );
