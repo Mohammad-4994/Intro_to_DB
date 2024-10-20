96336c94041e785d21f2cfbfa02c819b89f42de2
Mohammad Ali <mohammadaly494@gmail.com>
2024-10-20 07:51:18 +0200

    added task 4

--- task_4.sql
+++ task_4.sql
@@ -47,4 +47,16 @@ quantity DOUBLE
 );
 
 USE alx_book_store;
-SHOW TABLES ;
+
+SELECT 
+    COLUMN_NAME AS 'Column',
+    COLUMN_TYPE AS 'Type',
+    IS_NULLABLE AS 'Null',
+    COLUMN_KEY AS 'Key',
+    COLUMN_DEFAULT AS 'Default',
+    EXTRA AS 'Extra'
+FROM 
+    information_schema.COLUMNS
+WHERE 
+    TABLE_NAME = 'books'
+    AND TABLE_SCHEMA = DATABASE();
